9356a0ec1ed583ed854b53b0d641d9d7a59485bc
malsaslamcraft97 <aslam@incubyte.co>
2026-05-26 16:22:03 +0530

    test: TasksService.getAllTasks returns empty array - RED

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+
+describe('TasksService', () => {
+  let service: TasksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TasksService],
+    }).compile();
+
+    service = module.get<TasksService>(TasksService);
+  });
+
+  describe('getAllTasks', () => {
+    it('returns an empty array initially', () => {
+      expect(service.getAllTasks()).toEqual([]);
+    });
+  });
+});
